d392ecde6f24a70f1fdbaf6cbf68376be026a4ee
nix_arif <nix_arif@nixs-MacBook-Pro.local>
2025-12-10 11:42:31 +0800

    useSearchParam2

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,7 +1,13 @@
 import LoginForm from "@/components/auth/login-form";
+import { Suspense } from "react";
 
 const LoginPage = () => {
-  return <LoginForm />;
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}></Suspense>
+      <LoginForm />
+    </div>
+  );
 };
 
 export default LoginPage;
